5a384c6ff047ced4f24b14b285af688ec37f3f0d
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-02-11 00:43:08 +0900

    Add vim settings (#17)
    * Add vim settings
    
    * Change indent size
    
    * Update README.md
    
    * Update install.sh
    
    * Update README.md
    
    * Fix vimrc
    
    * Fix install.sh
    
    * Update README.md

--- README.md
+++ README.md
@@ -3,45 +3,61 @@
 
 注）`vim`コマンドで起動するエディタはNeovim
 ## インストール
+インストールされるNeovimのAppImageがx86-64用なので，その他のアーキテクチャでは代わりに簡易的なVimの設定が行われます．
 ```
 git clone https://github.com/amslabtech/dotfiles.git ~/dotfiles
 ~/dotfiles/install.sh
 ```
-以下がインストール（再インストール）されます
+以下がインストールされます
 - Neovim
+- Vim (vim-gtk)
 - tmux
 - xsel
 - python3
 - python3-pip
-- libfuse2 (ubuntu22.04のみ)
+- fuse
+- libfuse2
 - font (Hack Nerd Font Regular)
 
 以下がセットアップされます
 
 - Neovim
+- Vim
 - tmux
 
 ## トラブルシューティング
+#### インストールが途中で止まる
+- WSLの場合，pip installに時間がかかっている可能性が高いです．しばらく待つとインストールが進みます．
+- 操作を促すような表示がある場合，指示に従って下さい．（Enterの入力など）
 #### Neovimを起動すると画面が黒いまま動かない
 `Ctrl-c`を押した後，エディタを再起動して下さい
 
 #### Neovimのプラグインのインストールが開始されない
 エディタを再起動して下さい
 
+#### `vim`コマンドでNeovimではなく，Vimを起動したい
+以下を実行して下さい
+```
+sudo update-alternatives --auto vim
+```
+
+#### Vimのクリップボード連携が上手く行かない（アーキテクチャがx86_64以外）
+以下を実行し，`vim-gtk`を選択して下さい
+```
+sudo update-alternatives --config vim
+```
+
 #### 設定がおかしくなった
 再インストールすると，設定の上書きおよびキャッシュの削除が行われます
 
 #### 文字の色が白い
 True Colorに対応していない可能性があります<br>
-ssh接続している場合，自身の環境のtmux (`~/.tmux.conf`) のcolor設定を以下のようにして下さい
-
-(設定を変えた場合，tmuxを開き直す必要がある)
+ssh接続している場合，自身の環境のシェルの設定（`.bashrc`など）やtmux (`~/.tmux.conf`) のcolor設定を解除して下さい<br>
+それでも改善されない場合，シェルの設定に以下を追記して下さい
 ```
-set -g default-terminal "xterm-256color"
-set -g terminal-overrides "xterm:colors=256"
+export TERM=screen-256color
 ```
-
-shellでcolor設定している場合は解除して下さい
+※ 設定を変更した場合はターミナルまたはtmuxを再起動して下さい
 
 #### 文字の表示がおかしい
 - 端末のフォントが不適切な可能性があります
--- install.sh
+++ install.sh
@@ -5,27 +5,53 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # ####  Install  #####
 # ####################
 
+# curl
+echo "========================"
+echo " curl will be installed"
+echo "========================"
+sudo apt install curl -y --no-install-recommends
+echo ">>> Done"
+echo ""
+
+# wget
+echo "========================"
+echo " wget will be installed"
+echo "========================"
+sudo apt install wget -y --no-install-recommends
+echo ">>> Done"
+echo ""
+
 # font
 echo "=============================="
 echo " nerd-fonts will be installed"
 echo "=============================="
 if [ ! -d ~/.local/share/fonts ]; then
-    mkdir -p -v ~/.local/share/fonts
+  mkdir -p -v ~/.local/share/fonts
 fi
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/Hack/Regular/HackNerdFont-Regular.ttf
 echo ">>> Done"
 echo ""
 
-# neovim
-echo "=========================="
-echo " neovim will be installed"
-echo "=========================="
-sudo wget -O /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-sudo chmod +x /opt/nvim.appimage
-sudo ln -sf /opt/nvim.appimage /usr/bin/vim
-sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
+# vim
+echo "======================="
+echo " vim will be installed"
+echo "======================="
+sudo apt install vim-gtk -y --no-install-recommends
 echo ">>> Done"
 echo ""
+
+# neovim
+if [ $(arch) = "x86_64" ]; then
+  echo "=========================="
+  echo " neovim will be installed"
+  echo "=========================="
+  sudo wget -O /opt/nvim.appimage https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+  sudo chmod +x /opt/nvim.appimage
+  sudo ln -sf /opt/nvim.appimage /usr/bin/nvim
+  sudo ln -sf /usr/bin/nvim /usr/bin/vim
+  echo ">>> Done"
+  echo ""
+fi
 
 # tmux
 echo "=========================="
@@ -51,16 +77,21 @@ sudo apt install python3 python3-pip -y --no-install-recommends
 echo ">>> Done"
 echo ""
 
+# fuse
+echo "========================"
+echo " fuse will be installed"
+echo "========================"
+sudo apt install fuse -y --no-install-recommends
+echo ">>> Done"
+echo ""
+
 # libfuse2
-os_version=$(cat /etc/os-release | grep VERSION_ID | cut -d '"' -f 2)
-if [ $os_version == "22.04" ]; then
-    echo "============================"
-    echo " libfuse2 will be installed"
-    echo "============================"
-    sudo apt install libfuse2 -y --no-install-recommends
-    echo ">>> Done"
-    echo ""
-fi
+echo "============================"
+echo " libfuse2 will be installed"
+echo "============================"
+sudo apt install libfuse2 -y --no-install-recommends
+echo ">>> Done"
+echo ""
 
 
 # ##################
@@ -70,19 +101,32 @@ echo "======="
 echo " Setup"
 echo "======="
 
-# neovim
-echo "setting neovim... "
-if [ -d ~/.local/share/nvim/plugged ]; then
-    rm -rf ~/.local/share/nvim/plugged
+# vim/neovim
+echo "setting vim/neovim... "
+if [ -d ~/.vim/undo ]; then
+  rm -rf ~/.vim/undo
+fi
+if [ ! -d ~/.vim/undo ]; then
+  mkdir -pv ~/.vim/undo
+fi
+if [ -d ~/.vim/plugged ]; then
+  rm -rf ~/.vim/plugged
 fi
 if [ -d ~/.config/nvim/undo ]; then
-    rm -rf ~/.config/nvim/undo
+  rm -rf ~/.config/nvim/undo
 fi
 if [ ! -d ~/.config/nvim ]; then
-    mkdir -p -v ~/.config/nvim
+  mkdir -pv ~/.config/nvim
 fi
+ln -sf $SCRIPT_DIR/vimrc ~/.vimrc
 ln -sf $SCRIPT_DIR/init.vim ~/.config/nvim/init.vim
 pip3 install pynvim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -c 'PlugInstall --sync' -c qa
+if [ $(arch) = "x86_64" ]; then
+  vim -c 'UpdateRemotePlugins' -c qa
+fi
 echo ">>> Done"
 echo ""
 
@@ -90,7 +134,7 @@ echo ""
 echo "setting tmux... "
 ln -sf $SCRIPT_DIR/tmux.conf ~/.tmux.conf
 if [ -d ~/.tmux ]; then
-    rm -rf ~/.tmux
+  rm -rf ~/.tmux
 fi
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
