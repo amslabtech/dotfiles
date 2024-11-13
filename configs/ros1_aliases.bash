f85589443fddc4a3e373ec1b5946bc2f6d39a3db
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-11-13 14:03:05 +0900

    Update ROS settings (#26)
    * Update ROS settings
    
    * Update path

--- configs/ros1_aliases.bash
+++ configs/ros1_aliases.bash
@@ -1,21 +1,19 @@
 # Set alias commands for ROS
-if [[ -d /opt/ros ]]; then
+if [[ -d /opt/ros/noetic ]]; then
   alias cw='cd $ROS_WORKSPACE'
   alias cs='cd $ROS_WORKSPACE/src'
 
-  if [[ $ROS_VERSION -eq 1 ]]; then
-    function cmk_export_compile_commands_this() {
-      catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --this
-      if [[ -e package.xml ]]; then
-        local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
-        if [[ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]]; then
-          ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
-        else
-          echo "Error: compile_commands.json not found in build directory"
-        fi
+  function cmk_export_compile_commands_this() {
+    catkin build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --this
+    if [[ -e package.xml ]]; then
+      local package_name=$(cat package.xml | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
+      if [[ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]]; then
+        ln -sfv $ROS_WORKSPACE/build/$package_name/compile_commands.json ./compile_commands.json
       else
-        echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
+        echo "Error: compile_commands.json not found in build directory"
       fi
-    }
-  fi
+    else
+      echo "Error: package.xml not found in current directory, so can't create symlink to compile_commands.json"
+    fi
+  }
 fi
