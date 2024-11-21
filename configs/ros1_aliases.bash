e158f7f4aef1af842de422c9b7935bda457923bc
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-11-21 23:05:22 +0900

    Update format

--- configs/ros1_aliases.bash
+++ configs/ros1_aliases.bash
@@ -1,20 +1,20 @@
 # Set alias commands for ROS
 if [[ -d /opt/ros/noetic ]]; then
-  alias cw='cd $ROS_WORKSPACE'
-  alias cs='cd $ROS_WORKSPACE/src'
+  alias cw='cd ${ROS_WORKSPACE}'
+  alias cs='cd ${ROS_WORKSPACE}/src'
 
   function create_simple_compile_flags() {
-    echo "-I/opt/ros/$ROS_DISTRO/include" > compile_flags.txt
-    echo "-I$PWD/include" >> compile_flags.txt
+    echo "-I/opt/ros/noetic/include" > compile_flags.txt
+    echo "-I${PWD}/include" >> compile_flags.txt
   }
 
   function ln-compile-commands-json() {
     # Find compile_commands.json in build directory and create symlink to the top of the package directories.
-    local package_xmls=$(find $ROS_WORKSPACE/src -name package.xml)
+    local package_xmls=$(find ${ROS_WORKSPACE}/src -name package.xml)
     for package_xml in $(echo ${package_xmls}); do
       local package_name=$(cat ${package_xml} | grep '<name>' | awk -F '[>]' '{print $2}' | awk -F '[<]' '{print $1}')
-      if [[ -e $ROS_WORKSPACE/build/$package_name/compile_commands.json ]]; then
-        ln -sf $ROS_WORKSPACE/build/$package_name/compile_commands.json $(rospack find $package_name)/compile_commands.json
+      if [[ -e ${ROS_WORKSPACE}/build/${package_name}/compile_commands.json ]]; then
+        ln -sf ${ROS_WORKSPACE}/build/${package_name}/compile_commands.json $(rospack find ${package_name})/compile_commands.json
       fi
     done
   }
